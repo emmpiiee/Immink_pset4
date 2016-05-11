654ef1a8024b0cbb79040d8a22a67368f42eb8a4
emmpiiee <emmaimmink@hotmail.com>
2016-05-11 15:44:32 +0200

    Initial Commit

---
+++ Immink-pset4Tests/Immink_pset4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Immink_pset4Tests.swift
+//  Immink-pset4Tests
+//
+//  Created by Emma Immink on 11-05-16.
+//  Copyright © 2016 Emma Immink. All rights reserved.
+//
+
+import XCTest
+@testable import Immink_pset4
+
+class Immink_pset4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
